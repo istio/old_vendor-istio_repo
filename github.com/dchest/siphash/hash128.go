a4eb186992d43abb7cf4d75bf44ca63ffc54da19
Laurent Demailly <ldemailly@google.com>
2018-02-13 18:22:09 -0800

    dep ensure for istio master 17d6d49e7d007a3614161a8c4d26683050d39fa8

--- github.com/dchest/siphash/hash128.go
+++ github.com/dchest/siphash/hash128.go
@@ -1,23 +1,27 @@
 // +build !arm,!amd64 appengine gccgo
-
 // Written in 2012 by Dmitry Chestnykh.
+// Modifications 2014 for 128-bit hash function by Damian Gryski.
 //
-// To the extent possible under law, the author have dedicated all copyright
+// To the extent possible under law, the authors have dedicated all copyright
 // and related and neighboring rights to this software to the public domain
 // worldwide. This software is distributed without any warranty.
 // http://creativecommons.org/publicdomain/zero/1.0/
 
 package siphash
 
-// Hash returns the 64-bit SipHash-2-4 of the given byte slice with two 64-bit
+// Hash returns the 128-bit SipHash-2-4 of the given byte slice with two 64-bit
 // parts of 128-bit key: k0 and k1.
-func Hash(k0, k1 uint64, p []byte) uint64 {
+//
+// Note that 128-bit SipHash is considered experimental by SipHash authors at this time.
+func Hash128(k0, k1 uint64, p []byte) (uint64, uint64) {
 	// Initialization.
 	v0 := k0 ^ 0x736f6d6570736575
 	v1 := k1 ^ 0x646f72616e646f6d
 	v2 := k0 ^ 0x6c7967656e657261
 	v3 := k1 ^ 0x7465646279746573
 	t := uint64(len(p)) << 56
+
+	v1 ^= 0xee
 
 	// Compression.
 	for len(p) >= BlockSize {
@@ -134,7 +138,7 @@ func Hash(k0, k1 uint64, p []byte) uint64 {
 	v0 ^= t
 
 	// Finalization.
-	v2 ^= 0xff
+	v2 ^= 0xee
 
 	// Round 1.
 	v0 += v1
@@ -212,5 +216,87 @@ func Hash(k0, k1 uint64, p []byte) uint64 {
 	v1 ^= v2
 	v2 = v2<<32 | v2>>(64-32)
 
-	return v0 ^ v1 ^ v2 ^ v3
+	r0 := v0 ^ v1 ^ v2 ^ v3
+
+	v1 ^= 0xdd
+
+	// Round 1.
+	v0 += v1
+	v1 = v1<<13 | v1>>(64-13)
+	v1 ^= v0
+	v0 = v0<<32 | v0>>(64-32)
+
+	v2 += v3
+	v3 = v3<<16 | v3>>(64-16)
+	v3 ^= v2
+
+	v0 += v3
+	v3 = v3<<21 | v3>>(64-21)
+	v3 ^= v0
+
+	v2 += v1
+	v1 = v1<<17 | v1>>(64-17)
+	v1 ^= v2
+	v2 = v2<<32 | v2>>(64-32)
+
+	// Round 2.
+	v0 += v1
+	v1 = v1<<13 | v1>>(64-13)
+	v1 ^= v0
+	v0 = v0<<32 | v0>>(64-32)
+
+	v2 += v3
+	v3 = v3<<16 | v3>>(64-16)
+	v3 ^= v2
+
+	v0 += v3
+	v3 = v3<<21 | v3>>(64-21)
+	v3 ^= v0
+
+	v2 += v1
+	v1 = v1<<17 | v1>>(64-17)
+	v1 ^= v2
+	v2 = v2<<32 | v2>>(64-32)
+
+	// Round 3.
+	v0 += v1
+	v1 = v1<<13 | v1>>(64-13)
+	v1 ^= v0
+	v0 = v0<<32 | v0>>(64-32)
+
+	v2 += v3
+	v3 = v3<<16 | v3>>(64-16)
+	v3 ^= v2
+
+	v0 += v3
+	v3 = v3<<21 | v3>>(64-21)
+	v3 ^= v0
+
+	v2 += v1
+	v1 = v1<<17 | v1>>(64-17)
+	v1 ^= v2
+	v2 = v2<<32 | v2>>(64-32)
+
+	// Round 4.
+	v0 += v1
+	v1 = v1<<13 | v1>>(64-13)
+	v1 ^= v0
+	v0 = v0<<32 | v0>>(64-32)
+
+	v2 += v3
+	v3 = v3<<16 | v3>>(64-16)
+	v3 ^= v2
+
+	v0 += v3
+	v3 = v3<<21 | v3>>(64-21)
+	v3 ^= v0
+
+	v2 += v1
+	v1 = v1<<17 | v1>>(64-17)
+	v1 ^= v2
+	v2 = v2<<32 | v2>>(64-32)
+
+	r1 := v0 ^ v1 ^ v2 ^ v3
+
+	return r0, r1
 }
